377b3f230fc23209c314f2fa37d58c24c76c37c6
Jarkyn S <Jarkyn@Jarkyns-MacBook-Air.local>
2013-07-08 20:07:26 +0100

    create join table

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :image, :instructions, :title
+
+  has_and_belongs_to_many :ingredients
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708180045) do
+ActiveRecord::Schema.define(:version => 20130708185559) do
 
   create_table "ingredients", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "ingredients_recipes", :id => false, :force => true do |t|
+    t.integer "ingredient_id"
+    t.integer "recipe_id"
   end
 
   create_table "recipes", :force => true do |t|
